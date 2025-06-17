e4cfd212be646631bf904dd6d8cd16977bd9c9ff
mcDJIL <mdjauharil29@gmail.com>
2025-06-18 01:33:21 +0700

    slicing feature or service section

---
+++ resources/views/landing-page/components/feature.blade.php
@@ -0,0 +1,43 @@
+<div class="w-full pt-20 pb-20">
+    <div class="max-w-screen-xl w-full flex mx-auto px-6 sm:px-10">
+        <div class="w-full">
+            <div class="">
+                <h2 class="text-3xl text-green-800 sm:mb-3">Features or Services that help you</h2>
+                <h2 class="text-3xl text-green-800">to achieve your healthy life.</h2>
+            </div>
+    
+            <div class="mt-12 w-full flex flex-wrap justify-between gap-10">
+                <div class="w-full sm:w-[350px]">
+                    <div class="mb-3">
+                        <img src="{{ url('assets/images/healthy.png') }}" alt="">
+                    </div>
+    
+                    <div class="">
+                        <h3 class="font-semibold text-xl text-[#333333] mb-2">Meal customization</h3>
+                        <p>Customize your meals to match your taste, portions, and dietary preferences. Enjoy food that’s made for your lifestyle.</p>
+                    </div>
+                </div>
+                <div class="w-full sm:w-[350px]">
+                    <div class="mb-3">
+                        <img src="{{ url('assets/images/truck.png') }}" alt="">
+                    </div>
+    
+                    <div class="">
+                        <h3 class="font-semibold text-xl text-[#333333] mb-2">Delivery to major cities</h3>
+                        <p>Fresh, well-packaged meals delivered safely to major cities across Indonesia on time and ready to enjoy.</p>
+                    </div>
+                </div>
+                <div class="w-full sm:w-[350px]">
+                    <div class="mb-3">
+                        <img src="{{ url('assets/images/nutrition.png') }}" alt="">
+                    </div>
+    
+                    <div class="">
+                        <h3 class="font-semibold text-xl text-[#333333] mb-2">Detailed nutritional information</h3>
+                        <p>Get full nutrition facts for every dish, including calories, macros, and ingredients—helping you stay informed and on track.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
